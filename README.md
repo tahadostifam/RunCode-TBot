dd5c45f7155898905d625cb7d8d1a4400266c997
[ TaHa. DostifaM  ] <mr.tahadostifam@gmail.com>
2022-04-02 21:30:26 +0430

    Init for README.md & bug fix on main.go

---
+++ README.md
@@ -0,0 +1,8 @@
+### Configs
+```txt
+BOT_TOKEN: 
+SSH_HOST: 
+SSH_PORT: 
+SSH_USER: 
+SSH_PASS: 
+```
--- main.go
+++ main.go
@@ -5,29 +5,27 @@ import (
 	"CODE-Runner/configs"
 	"fmt"
 	"os"
-	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-var token string = strings.TrimSpace(os.Getenv("CODE_RUNNER_BOT_TOKEN"))
+var token string
 
 func main() {
-	if len(token) > 0 {
-		configs.ReadAndSetConfigs()
-		code_runner_bot.SetSSHClient()
 
-		bot_client, bot_err := tgbotapi.NewBotAPI(token)
-		if bot_err != nil {
-			fmt.Printf("Error: %v\n", bot_err)
-			os.Exit(1)
-		}
+	configs.ReadAndSetConfigs()
+	code_runner_bot.SetSSHClient()
 
-		fmt.Println("Welcome to CODE-Runner Bot")
+	token = configs.AllConfigs.BOT_TOKEN
 
-		code_runner_bot.RunBot(bot_client)
-
-	} else {
-		fmt.Println("Bot token not found. Please set token using :\nexport CODE_RUNNER_BOT_TOKEN=your-token")
+	bot_client, bot_err := tgbotapi.NewBotAPI(token)
+	if bot_err != nil {
+		fmt.Printf("Error: %v\n", bot_err)
+		os.Exit(1)
 	}
+
+	fmt.Println("Welcome to CODE-Runner Bot")
+
+	code_runner_bot.RunBot(bot_client)
+
 }
